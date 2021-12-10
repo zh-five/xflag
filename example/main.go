a848fad09d6de7e914410fb7a053e3cfe819da06
肖武 <lh_ts24@qq.com>
2021-12-10 14:29:01 +0800

    基本完成

---
+++ example/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/zh-five/xflag"
+)
+
+func main() {
+	flag := xflag.Flag()
+
+	flag.Desc("用法: "+flag.Name()+" [cmd...] [option...] [param...]", "\n作者: xxx")
+
+	// 绑定子命令
+	flag.BindCmd("clone", cmdClone, "克隆资源")
+	flag.BindCmd("add", cmdAdd, "添加文件内容到索引")
+	flag.BindCmd("help", nil, "查看帮助信息")
+
+	help := flag.Bool("h", false, "查看帮助信息")
+	flag.Parse()
+
+	if *help || flag.CmdName == "" || flag.CmdName == "help" {
+		flag.Usage()
+	}
+}
+
+func cmdClone(flag *xflag.XFlagSet) {
+	flag.Desc("用法: "+flag.Name()+" [-b 分支] repo [dir]", "")
+	branch := flag.String("b", "", "分支名称")
+	flag.Parse()
+
+	repo := flag.Arg(0) //仓库
+	dir := flag.Arg(1)  //目录
+
+	if repo == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fmt.Printf("branch: %s, repo: %s, dir: %s\n", *branch, repo, dir)
+}
+
+func cmdAdd(flag *xflag.XFlagSet) {
+	flag.Desc("添加文件到索引\n用法: "+flag.Name()+" [-f] <文件列表>", "")
+	force := flag.Bool("f", false, "force, 允许添加忽略的文件")
+	flag.Parse()
+
+	files := flag.Args() //文件列表
+	if len(files) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fmt.Println("force:", *force, "files:", files)
+}
--- xflag.go
+++ xflag.go
@@ -8,8 +8,9 @@ import (
 
 type XFlagSet struct {
 	*flag.FlagSet
-	level uint                //当前子命令级别, 顶层为1
-	cmds  map[string]*cmdInfo //下一级子命令信息列表
+	level   uint                //当前子命令级别, 顶层为1 , Parse(os.Arg[1:])
+	cmds    map[string]*cmdInfo //下一级子命令信息列表
+	CmdName string              //被执行的子命令
 }
 
 type cmdInfo struct {
@@ -22,60 +23,72 @@ type CmdFunc func(flag *XFlagSet)
 //根节点
 var root *XFlagSet = newXFlagSet(os.Args[0], 1)
 
+func Flag() *XFlagSet {
+	if root.Parsed() {
+		return nil
+	}
+	return root
+}
+
 func newXFlagSet(name string, level uint) *XFlagSet {
-	return &XFlagSet{
+	x := &XFlagSet{
 		level:   level,
 		FlagSet: flag.NewFlagSet(name, flag.ExitOnError),
 		cmds:    make(map[string]*cmdInfo),
 	}
+
+	usage := x.FlagSet.Usage
+	x.FlagSet.Usage = func() {
+		x.cmdList()
+		usage()
+	}
+
+	return x
 }
 
-// 子命令绑定
-func SubCmd(name, desc string, cb CmdFunc) {
-	root.SubCmd(name, desc, cb)
-}
-
-// 执行解析
-func Parse() {
-	root.Parse()
-}
-
-func (x *XFlagSet) SubCmd(name, desc string, cb CmdFunc) {
+// 绑定子命令处理函数
+func (x *XFlagSet) BindCmd(name string, cb CmdFunc, desc string) {
 	x.cmds[name] = &cmdInfo{
 		desc: desc,
 		cb:   cb,
 	}
 }
 
+// 解析
 func (x *XFlagSet) Parse() {
-	h1 := x.Bool("h", false, "查看帮助")
-	h2 := x.Bool("help", false, "查看帮助")
-
 	// 有配置子命令
 	if len(x.cmds) > 0 && len(os.Args) > int(x.level) { // 参数里有子命令
 		s_cmd := os.Args[x.level]
 		if info, ok := x.cmds[s_cmd]; ok { // 子命令合法
-			tmp := newXFlagSet(x.Name()+" "+s_cmd, x.level+1)
-			info.cb(tmp)
-			return
+			x.CmdName = s_cmd
+			if info.cb != nil {
+				tmp := newXFlagSet(x.Name()+" "+s_cmd, x.level+1)
+				info.cb(tmp)
+			}
 		}
 	}
 
 	x.FlagSet.Parse(os.Args[x.level:]) //解析参数
-	if *h1 || *h2 {
-		x.PrintDefaults()
-		os.Exit(0)
-	}
-
 }
 
-func (x *XFlagSet) PrintDefaults() {
+func (x *XFlagSet) Desc(top, bottom string) {
+	usage := x.FlagSet.Usage
+	x.FlagSet.Usage = func() {
+		if top != "" {
+			fmt.Println(top)
+		}
+		usage()
+		if bottom != "" {
+			fmt.Println(bottom)
+		}
+	}
+}
+
+func (x *XFlagSet) cmdList() {
 	if len(x.cmds) > 0 {
-		fmt.Fprintln(x.Output(), "Subcommand of", x.Name())
+		fmt.Fprintln(x.Output(), "Subcommand of", x.Name()+":")
 		for k, v := range x.cmds {
 			fmt.Fprintln(x.Output(), "    ", k, "\t", v.desc)
 		}
 	}
-
-	x.Usage()
 }
